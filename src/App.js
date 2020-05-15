bc976c2c059bf35fddfde225ebabfdd0888e09a1
Shubz Kothekar <account@shubzkothekar.com>
2020-05-15 17:59:05 +0530

    added chart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Cards, Charts, CountryPicker } from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
+
+
 class App extends React.Component {
 
     state = {
@@ -17,9 +19,10 @@ class App extends React.Component {
 
         return (
             <div >
-                <Cards className={styles.container} data={data} />
-                <Charts />
+                <Cards className={styles.cards} data={data} />
                 <CountryPicker />
+                <Charts className={styles.charts} />
+
 
             </div>
         )
