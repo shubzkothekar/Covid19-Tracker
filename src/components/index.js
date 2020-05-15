47aaf3e1f8a0cfe57a2e2bc033d5a61c093b399e
Shubz Kothekar <account@shubzkothekar.com>
2020-05-15 23:02:57 +0530

    bug fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Cards } from './Cards/Cards'
-export { default as Charts } from './Charts/Charts'
-export { default as CountryPicker } from './CountryPicker/CountryPicker'
+export { default as Cards } from './Cards/Cards.jsx'
+export { default as Charts } from './Charts/Charts.jsx'
+export { default as CountryPicker } from './CountryPicker/CountryPicker.jsx'
